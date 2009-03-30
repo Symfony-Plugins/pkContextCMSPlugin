91fd0935a19191983c73e5ef91334658d48b4780
johnnyoffline <johnnyoffline@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-03-30 16:32:43 -0500

    Lucene generated files
    git-svn-id: http://svn.symfony-project.com/plugins/pkContextCMSPlugin/trunk@16778 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/filter/doctrine/PluginpkContextCMSLuceneUpdateFormFilter.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * PluginpkContextCMSLuceneUpdate form.
+ *
+ * @package    filters
+ * @subpackage pkContextCMSLuceneUpdate *
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+abstract class PluginpkContextCMSLuceneUpdateFormFilter extends BasepkContextCMSLuceneUpdateFormFilter
+{
+}
