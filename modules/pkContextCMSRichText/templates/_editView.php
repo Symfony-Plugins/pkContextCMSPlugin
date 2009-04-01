6af4402f2e6609811e20829091f04804c14e8203
boutell <boutell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-04-01 09:06:43 -0500

    Missing a 'var'
    git-svn-id: http://svn.symfony-project.com/plugins/pkContextCMSPlugin/trunk@16859 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkContextCMSRichText/templates/_editView.php
+++ modules/pkContextCMSRichText/templates/_editView.php
@@ -6,7 +6,7 @@ pkContextCMS.registerOnSubmit("<?php echo $id ?>",
   function(slotId)
   {
     <?php # FCK doesn't do this automatically on an AJAX "form" submit ?>
-    value = FCKeditorAPI.GetInstance('value-<?php echo $id ?>').GetXHTML();
+    var value = FCKeditorAPI.GetInstance('value-<?php echo $id ?>').GetXHTML();
     $('#value-<?php echo $id ?>').val(value);
   }
 );
