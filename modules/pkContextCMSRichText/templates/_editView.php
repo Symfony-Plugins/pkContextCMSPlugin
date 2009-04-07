0ac757741e4970fa0c098927da2390d06ae0245f
boutell <boutell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-04-07 10:22:42 -0500

    Rich text editor uses pk_textarea_tag to work around Symfony's bug with Safari and Chrome (not the other way round Safari and Chrome are not to blame for saying "newlines? In MY <input type="text" />?")
    git-svn-id: http://svn.symfony-project.com/plugins/pkContextCMSPlugin/trunk@17098 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkContextCMSRichText/templates/_editView.php
+++ modules/pkContextCMSRichText/templates/_editView.php
@@ -1,6 +1,12 @@
-<?php echo textarea_tag("value-$id",
-  $value,
+<?php // We can start calling regular textarea_tag when they fix ?>
+<?php // http://trac.symfony-project.com/ticket/732 ?>
+
+<?php use_helper('PkForm') ?>
+<?php $tag = pk_textarea_tag("value-$id",
+  $value, 
   array_merge($options, array("rich" => "fck"))) ?>
+<?php $tag = str_replace('type="text"', 'type="hidden"', $tag) ?>
+<?php echo $tag ?>
 <script>
 pkContextCMS.registerOnSubmit("<?php echo $id ?>", 
   function(slotId)
