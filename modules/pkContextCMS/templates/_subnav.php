811aa3eacb35429f8a2fb5a307e4d4ab1e3beaa9
johnnyoffline <johnnyoffline@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-06-02 17:08:27 -0500

    Subnav
    git-svn-id: http://svn.symfony-project.com/plugins/pkContextCMSPlugin/trunk@18868 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkContextCMS/templates/_subnav.php
+++ modules/pkContextCMS/templates/_subnav.php
@@ -1,9 +1,5 @@
 <?php use_helper('pkContextCMS') ?>
 
-<div id="subnav" class="subnav shadow">
-		<div class="content-container">
-			<div class="content">
-	    <?php echo pk_context_cms_navcolumn(false) ?>
-			</div>
-		</div>
-</div>			
+<div id="pk-subnav" class="subnav">
+<?php echo pk_context_cms_navcolumn(false) ?>
+</div>
