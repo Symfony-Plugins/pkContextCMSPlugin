037305a34e1fe51165262e8f16b70d0889190188
rickybanister <rickybanister@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-06-15 19:29:00 -0500

    fixed search box markup and id names
    git-svn-id: http://svn.symfony-project.com/plugins/pkContextCMSPlugin/trunk@19292 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkContextCMS/templates/_search.php
+++ modules/pkContextCMS/templates/_search.php
@@ -1,3 +1,3 @@
-<form id="pk-context-cms-search-pages" action="<?php echo url_for("pkContextCMS/search") ?>" method="GET">
+<form id="pk-search-global" action="<?php echo url_for("pkContextCMS/search") ?>" method="GET" class="pk-search-form">
 <input type="text" name="q" value="<?php echo htmlspecialchars($sf_request->getParameter('q')) ?>"/> <input type="submit" class="submit" value="Search Pages" />
 </form>
