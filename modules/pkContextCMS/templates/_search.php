21c3126213a8e3ff63bc2c69e76942e6259b41c4
rickybanister <rickybanister@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-07-21 15:30:15 -0500

    updated search markup to match trinity
    git-svn-id: http://svn.symfony-project.com/plugins/pkContextCMSPlugin/trunk@20385 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkContextCMS/templates/_search.php
+++ modules/pkContextCMS/templates/_search.php
@@ -1,3 +1,4 @@
 <form id="pk-search-global" action="<?php echo url_for("pkContextCMS/search") ?>" method="GET" class="pk-search-form">
-<input type="text" name="q" value="<?php echo htmlspecialchars($sf_request->getParameter('q')) ?>" class="pk-search-field"/> <input type="submit" class="submit" value="Search Pages" />
+<input type="text" name="q" value="<?php echo htmlspecialchars($sf_request->getParameter('q')) ?>" class="pk-search-field"/> 
+<input type="image" src="/pkContextCMSPlugin/images/pk-special-blank.gif" class="submit" value="Search Pages" />
 </form>
