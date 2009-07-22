6766b31ee473bc9526cd1d7584fae050d6dac0a0
boutell <boutell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-07-22 09:39:30 -0500

    whitespace only
    git-svn-id: http://svn.symfony-project.com/plugins/pkContextCMSPlugin/trunk@20417 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkContextCMS/templates/_simpleEditButton.php
+++ modules/pkContextCMS/templates/_simpleEditButton.php
@@ -8,10 +8,8 @@
 	)) ?>
 	<script type="text/javascript">
 	$(document).ready(function(){
-		
 		var editBtn = $('#pk-slot-edit-<?php echo $name ?>-<?php echo $permid ?>');
 		var editSlot = $('#pk-slot-<?php echo $name ?>-<?php echo $permid ?>');
-		
 		editBtn.click(function(event){
 			$(this).parent().addClass('editing-now');
 			$(editSlot).children('.pk-slot-content').children('.pk-slot-content-container').hide(); // Hide content
