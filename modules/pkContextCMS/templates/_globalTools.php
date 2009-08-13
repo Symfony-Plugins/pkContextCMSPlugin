04e9ebec06dc0f7a42cce3f550eddf73deed2135
johnnyoffline <johnnyoffline@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-08-13 12:23:21 -0500

    Rewrote javascript so that the apostrophe toggles properly
    git-svn-id: http://svn.symfony-project.com/plugins/pkContextCMSPlugin/trunk@21140 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkContextCMS/templates/_globalTools.php
+++ modules/pkContextCMS/templates/_globalTools.php
@@ -13,14 +13,16 @@ These are mostly links to independent modules.
   <?php // All logged in users, including guests with no admin abilities, need access to the ?>
   <?php // logout button. But if you have no legitimate admin roles, you shouldn't see the ?>
   <?php // apostrophe or the global buttons ?>
+
   <?php $buttons = pkContextCMSTools::getGlobalButtons() ?>
   <?php $page = pkContextCMSTools::getCurrentPage() ?>
   <?php $pageEdit = $page && $page->userHasPrivilege('edit') ?>
   <?php $cmsAdmin = $sf_user->hasCredential('cms_admin') ?>
+
   <?php if ($cmsAdmin || count($buttons) || $pageEdit): ?>
 
   	<?php //The Apostrophe ?>
-  	<li>
+  	<li class="pk-global-toolbar-apostrophe">
   		<?php echo jq_link_to_function('Apostrophe Now','',array('id' => 'the-apostrophe', )) ?>
   		<ul class="pk-global-toolbar-buttons pk-controls">
   			<?php $buttons = pkContextCMSTools::getGlobalButtons() ?>
@@ -33,138 +35,67 @@ These are mostly links to independent modules.
 
   	<?php //Breadcrumb ?>
   	<?php if (pkContextCMSTools::getCurrentPage()): ?>
-  	<li>
-  		<?php include_component('pkContextCMS', 'breadcrumb') # Breadcrumb Navigation ?>
-  	</li>
+	  	<li class="pk-global-toolbar-breadcrumb">
+	  		<?php include_component('pkContextCMS', 'breadcrumb') # Breadcrumb Navigation ?>
+	  	</li>
   	<?php endif ?>
 
-  	<li class="pk-page-settings-container">
+  	<li class="pk-global-toolbar-page-settings pk-page-settings-container">
   		<div id="pk-page-settings"></div>
   	</li>
-  	<li class="pk-personal-settings-container">
-            <div id="pk-personal-settings"></div>
-    </li>  
+
+  	<li class="pk-global-toolbar-user-settings pk-personal-settings-container">
+			<div id="pk-personal-settings"></div>
+    </li>
+
 	<?php endif ?>
-	<?php // also log out button and settings button ?>
-	<li class="pk-login">
-		<?php include_partial("pkContextCMS/login") ?>
-	</li>
+
+		<?php // Login / Logout ?>
+		<li class="pk-global-toolbar-login pk-login">
+			<?php include_partial("pkContextCMS/login") ?>
+		</li>
 </ul>
 
 <script type="text/javascript">
 
-	var apostropheOpenState = 0;
-
-	function apostropheOpen() 
-	{
-		$(this).parent().siblings().hide();
-		$('.pk-global-toolbar-buttons').fadeIn();
-		$('.pk-global-toolbar-buttons .pk-cancel').fadeIn();			
-		$('.pk-global-toolbar-buttons .pk-cancel').parent().show();
-		apostropheOpenState = 1;
-	}
-
-	function apostropheClose() 
-	{
-	  $(this).parent().siblings().fadeIn();
-	  $('.pk-global-toolbar-buttons').hide();			
-		apostropheOpenState = 0;
-	}
-
 	$(document).ready(function(){
+
+		var aposToggle = 0;
+
 	  $('#the-apostrophe').click(function(){
+		
+			if (!aposToggle)
+			{
+				$(this).addClass('open');
+				$('.pk-global-toolbar-breadcrumb').hide();
+				$('.pk-global-toolbar-buttons').fadeIn();
+				$('.pk-global-toolbar-buttons .pk-cancel').fadeIn();			
+				$('.pk-global-toolbar-buttons .pk-cancel').parent().show();
+				aposToggle = 1;
+			}
+			else
+			{
+				closeApostrophe();				
+				aposToggle = 0;
+			}
 
-	   if (!apostropheOpenState)
-	   {
-	     apostropheOpen();
-	   }
-	   else
-	   {
-	     apostropheClose();
-	   }
-
-		$(this).toggleClass('open');
-
-	  });
+		});
   
-		$('.pk-global-toolbar-buttons .pk-cancel').click(function(){
-	  $(this).parent().parent().hide();
-	  $(this).parent().parent().parent().siblings().fadeIn();
-	  apostropheOpenState = 0;
-	
+		$('.pk-global-toolbar-apostrophe .pk-cancel').click(function(){
+			closeApostrophe();
+			aposToggle = 0;
 	  });      
 
+		function closeApostrophe()
+		{
+			$('#the-apostrophe').removeClass('open');
+			$('.pk-global-toolbar-buttons').hide();			
+			$('.pk-global-toolbar-breadcrumb').fadeIn();
+		}
+
 	});
-	
+
 </script>
-
-<?php // TODO: Rewrite this with jQuery so it's lighter! ?>
-<?php if (0): ?>
-  
-<script type="text/javascript">
-var prevColorRed = randomInteger(256);
-var prevColorGreen = randomInteger(256);
-var prevColorBlue = randomInteger(256);
-setNextColor();
-var nextColorRed = prevColorRed;
-var nextColorGreen = prevColorGreen;
-var nextColorBlue = prevColorBlue;
-var colorSteps = 50;
-var colorStep = 0;
-transition();
-
-function transition()
-{
-  prevColorRed = nextColorRed;
-  prevColorGreen = nextColorGreen;
-  prevColorBlue = nextColorBlue;
-  setNextColor();
-  colorStep = 0;
-}
-
-function setNextColor()
-{
-  nextColorRed = randomInteger(256);
-  nextColorGreen = randomInteger(256);
-  nextColorBlue = randomInteger(256);
-}
-
-function interpolate(prev, next)
-{
-  return prev + (next - prev) * colorStep / colorSteps;
-}
-
-function tohex(n)
-{
-  return (n >> 4).toString(16) + (n & 15).toString(16);
-}
-
-function randomInteger(n)
-{
-  return Math.floor(Math.random() * n);
-}
-
-setInterval(
-  function() 
-  {
-    var strobeme = document.getElementById('the-apostrophe');
-    var b = '#';
-    var i;
-    var red = interpolate(prevColorRed, nextColorRed);
-    var green = interpolate(prevColorGreen, nextColorGreen);
-    var blue = interpolate(prevColorBlue, nextColorBlue);
-    b += tohex(red);
-    b += tohex(green);
-    b += tohex(blue);
-    strobeme.style.backgroundColor = b;
-    colorStep++;
-    if (colorStep == colorSteps)
-    {
-      transition();
-    }
-  }, 20);
-</script>
-<?php endif ?>
 
 <?php if (pkContextCMSTools::getCurrentPage()): ?>
 	<?php include_partial('pkContextCMS/historyBrowser') ?>
