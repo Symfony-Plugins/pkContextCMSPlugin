76627ecf8a4784b8d29e80955ec0fe787da6f8ea
boutell <boutell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-09-17 10:38:11 -0500

    fixed 'directions' option
    git-svn-id: http://svn.symfony-project.com/plugins/pkContextCMSPlugin/trunk@22107 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkContextCMSRawHTML/templates/_editView.php
+++ modules/pkContextCMSRawHTML/templates/_editView.php
@@ -1,5 +1,12 @@
 <p>
-Use this slot to add raw HTML markup, such as embed codes. Use this
+<?php if (isset($options['directions'])): ?>
+  <?php echo $options['directions'] ?>
+<?php else: ?>
+  Use this slot to add raw HTML markup, such as embed codes. 
+<?php endif ?>
+</p>
+<p>
+Use this
 slot with caution. If bad markup causes the page to become uneditable, add ?safemode=1 to the URL
 and edit the slot to correct the markup.
 </p>
