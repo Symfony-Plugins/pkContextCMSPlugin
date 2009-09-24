b0bb2900d8f51e85cb5e0f00e635ab31145623c1
johnnyoffline <johnnyoffline@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-09-24 16:06:03 -0500

    some styling for rawHTML Slot. slot is broken needs some Tom love <3
    git-svn-id: http://svn.symfony-project.com/plugins/pkContextCMSPlugin/trunk@22392 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkContextCMSRawHTML/templates/_editView.php
+++ modules/pkContextCMSRawHTML/templates/_editView.php
@@ -1,30 +1,25 @@
-<p>
-<?php if (isset($options['directions'])): ?>
-  <?php echo $options['directions'] ?>
-<?php else: ?>
-  Use this slot to add raw HTML markup, such as embed codes. 
-<?php endif ?>
-</p>
-<p>
-Use this
-slot with caution. If bad markup causes the page to become uneditable, add ?safemode=1 to the URL
-and edit the slot to correct the markup.
-</p>
+<ul class="pk-raw-html-info">
+	<li>
+		<?php if (isset($options['directions'])): ?>
+	  	<?php echo $options['directions'] ?>
+		<?php else: ?>
+	  	Use this slot to add raw HTML markup, such as embed codes. 
+		<?php endif ?>
+	</li>
+	<li>
+		Use this slot with caution. If bad markup causes the page to become uneditable, add ?safemode=1 to the URL and edit the slot to correct the markup.
+	</li>
+</ul>
 
 <?php echo textarea_tag("value", $value, 
-  html_entity_decode(strip_tags($value)),
-  array_merge(array("id" => "$id-value", 'class' => 'pkContextCMSRawHTMLSlot'), $options)) ?>
+	html_entity_decode(strip_tags($value)),
+		array_merge(array("id" => "$id-value", 'class' => 'pkContextCMSRawHTMLSlot'), $options)) ?>
 
 <script type="text/javascript">
-
-$(document).ready (function() {
-	
-	$('textarea.pkContextCMSRawHTMLSlot').autogrow({
-		// maxHeight: 400, //Max Height was causing problems.
-		minHeight: 30,
-		lineHeight: 16
+	$(document).ready (function() {
+		$('textarea.pkContextCMSRawHTMLSlot').autogrow({
+			minHeight: 64,
+			lineHeight: 16
+		});
 	});
-							
-});
-
 </script>
--- modules/pkContextCMSRawHTML/templates/_normalView.php
+++ modules/pkContextCMSRawHTML/templates/_normalView.php
@@ -2,20 +2,18 @@
   array('name' => $name, 'permid' => $permid)) ?>
 
 <?php if (!strlen($value)): ?>
+<ul class="pk-raw-html-info">
   <?php if ($editable): ?>
-    <p>
+    <li>
     <?php if (isset($options['directions'])): ?>
       <?php echo $options['directions'] ?>
     <?php else: ?>
       Click edit to add raw HTML markup, such as embed codes. 
     <?php endif ?>
-    </p>
-    <p>
-    Use this
-    slot with caution. If bad markup causes the page to become uneditable, add ?safemode=1 to the URL
-    and edit the slot to correct the markup.
-    </p>
+    </li>
+    <li>Use this slot with caution. If bad markup causes the page to become uneditable, add ?safemode=1 to the URL and edit the slot to correct the markup.</li>
   <?php endif ?>
+</ul>
 <?php else: ?>
   <?php if ($sf_params->get('safemode')): ?>
     <?php echo htmlspecialchars($value) ?>
