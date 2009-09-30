647ca5f17123db4637b7d2d0e28504194cfe531a
johnnyoffline <johnnyoffline@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-09-30 13:10:07 -0500

    added jquery.pulse.js to the toolkit js library - going to use it for highlights for ajax events etc.
    git-svn-id: http://svn.symfony-project.com/plugins/pkContextCMSPlugin/trunk@22639 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/pkContextCMSHelper.php
+++ lib/helper/pkContextCMSHelper.php
@@ -23,7 +23,8 @@ function _pk_context_cms_required_assets()
   $response->addJavascript('/pkToolkitPlugin/js/pkUI.js');
   $response->addJavascript('/pkToolkitPlugin/js/pkControls.js');
   $response->addJavascript('/pkToolkitPlugin/js/jquery.hotkeys-0.7.9.min.js'); // this is plugin for hotkey toggle for cms UI
-  $response->addJavascript('/pkToolkitPlugin/js/jquery.autogrow.js');
+  $response->addJavascript('/pkToolkitPlugin/js/jquery.autogrow.js'); // Autogrowing Textareas
+  $response->addJavascript('/pkToolkitPlugin/js/jquery.pulse.js'); // Ajax update highlight a color
   $webDir = sfConfig::get('sf_pkContextCMS_web_dir', '/pkContextCMSPlugin');
   $response->addJavascript("$webDir/js/pkContextCMS.js");
 
