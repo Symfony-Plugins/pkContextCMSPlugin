39511c65066a014eacc9cf569932d859e121d9d1
boutell <boutell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-11-25 10:47:53 -0600

    1.3 compatibility tweaks. Should be backwards compatible to 1.2
    git-svn-id: http://svn.symfony-project.com/plugins/pkContextCMSPlugin/trunk@24384 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkContextCMS/templates/_ajaxUpdateSlot.php
+++ modules/pkContextCMS/templates/_ajaxUpdateSlot.php
@@ -1,3 +1,7 @@
+<?php // 1.3 and up don't do this automatically (no common filter) ?>
+<?php // We're using renderPartial so there is no layout to call this for us ?>
+<?php include_javascripts() ?>
+<?php include_stylesheets() ?>
 <?php use_helper('pkContextCMS') ?>
 <?php pk_context_cms_slot_body($name, $type, $permid, 
   $options, $validationData, $editorOpen) ?>
