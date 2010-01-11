047c6d3aa62245cc81fdf8b68f52974b0747f948
johnnyoffline <johnnyoffline@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-01-11 15:16:35 -0600

    Fixed bug where javascript was being output for admin specific scenarios that shouldn't be there. Also enhanced slideshow to display the first image if nojavascript
    git-svn-id: http://svn.symfony-project.com/plugins/pkContextCMSPlugin/trunk@26489 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkContextCMS/templates/_renamePage.php
+++ modules/pkContextCMS/templates/_renamePage.php
@@ -21,85 +21,85 @@
 
   </form>
 
+	<script type="text/javascript" charset="utf-8">
+		$(document).ready(function() {
+
+			var renameForm = $('.pk-breadcrumb-form.rename')
+			renameForm.prepend('<b class="pk-breadcrumb-rename-title-spacer" style="display:none;float:left;white-space:nowrap;"><?php echo $page->getTitle() ?></b>')
+
+			var renameControls = $('#pk-breadcrumb-rename-controls');
+			var renameSpacer = $('.pk-breadcrumb-rename-title-spacer');
+			var renameInput = $('#pk-breadcrumb-rename-title');
+			var renameInputWidth = checkInputWidth(renameSpacer.width());		
+			renameInput.css('width', renameInputWidth);		
+
+			var currentTitle = "<?php echo $page->getTitle() ?>";
+			renameInput[0].value = currentTitle;
+			var liveTitle = renameInput[0].value;
+
+
+			renameInput.bind('cancel', function(e){
+				renameSpacer.text(currentTitle)
+				renameInput[0].value = currentTitle;
+				renameInputWidth = checkInputWidth(renameSpacer.width());
+				renameInput.css('width', renameInputWidth);
+				renameControls.hide();
+				renameInput.blur();
+			});
+
+			renameInput.focus(function(){
+				renameControls.fadeIn();
+				renameInput.select();
+			})
+
+			renameInput.blur(function(){
+				renameControls.hide();
+			})
+
+			renameInput.keydown(function(e){
+				liveTitle = renameInput[0].value;
+				renameSpacer.text(liveTitle);
+			});
+
+			renameInput.keyup(function(e){
+				if ($.charcode(e) == 'escape')
+				{
+					renameInput.trigger('cancel');
+				}			
+				renameInputWidth = checkInputWidth(renameSpacer.width());
+				renameInput.css('width', renameInputWidth);
+			});
+
+			renameControls.find('.pk-cancel').click(function(){
+				renameInput.trigger('cancel');
+			});
+
+			function checkInputWidth(w)
+			{
+				var minWidth = 50;
+				var maxWidth = 250;
+				if (w < minWidth)
+				{
+					return minWidth;
+				} 
+				else if (w > maxWidth)
+				{
+					// we are not enforcing maxWidth at the moment;
+					// return maxWidth;
+					return w;
+				}
+				else
+				{
+					return w;
+				}
+			}
+
+		});
+
+	</script>
+
 <?php else: ?>
 
   <?php echo $page->getTitle() ?>
 
 <?php endif ?>
-
-<script type="text/javascript" charset="utf-8">
-	$(document).ready(function() {
-		
-		var renameForm = $('.pk-breadcrumb-form.rename')
-		renameForm.prepend('<b class="pk-breadcrumb-rename-title-spacer" style="display:none;float:left;white-space:nowrap;"><?php echo $page->getTitle() ?></b>')
-
-		var renameControls = $('#pk-breadcrumb-rename-controls');
-		var renameSpacer = $('.pk-breadcrumb-rename-title-spacer');
-		var renameInput = $('#pk-breadcrumb-rename-title');
-		var renameInputWidth = checkInputWidth(renameSpacer.width());		
-		renameInput.css('width', renameInputWidth);		
-		
-		var currentTitle = "<?php echo $page->getTitle() ?>";
-		renameInput[0].value = currentTitle;
-		var liveTitle = renameInput[0].value;
-		
-					
-		renameInput.bind('cancel', function(e){
-			renameSpacer.text(currentTitle)
-			renameInput[0].value = currentTitle;
-			renameInputWidth = checkInputWidth(renameSpacer.width());
-			renameInput.css('width', renameInputWidth);
-			renameControls.hide();
-			renameInput.blur();
-		});
-		
-		renameInput.focus(function(){
-			renameControls.fadeIn();
-			renameInput.select();
-		})
-		
-		renameInput.blur(function(){
-			renameControls.hide();
-		})
-		
-		renameInput.keydown(function(e){
-			liveTitle = renameInput[0].value;
-			renameSpacer.text(liveTitle);
-		});
-
-		renameInput.keyup(function(e){
-			if ($.charcode(e) == 'escape')
-			{
-				renameInput.trigger('cancel');
-			}			
-			renameInputWidth = checkInputWidth(renameSpacer.width());
-			renameInput.css('width', renameInputWidth);
-		});
-		
-		renameControls.find('.pk-cancel').click(function(){
-			renameInput.trigger('cancel');
-		});
-		
-		function checkInputWidth(w)
-		{
-			var minWidth = 50;
-			var maxWidth = 250;
-			if (w < minWidth)
-			{
-				return minWidth;
-			} 
-			else if (w > maxWidth)
-			{
-				// we are not enforcing maxWidth at the moment;
-				// return maxWidth;
-				return w;
-			}
-			else
-			{
-				return w;
-			}
-		}
-		
-	});
-	
-</script>
